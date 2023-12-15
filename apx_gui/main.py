4ee1ae996a8578a73f63c7825968064e34dba9f2
mirkobrombin <send@mirko.pm>
2023-12-15 23:06:18 +0100

    feat: built-in console

--- apx_gui/main.py
+++ apx_gui/main.py
@@ -24,6 +24,7 @@ from gettext import gettext as _
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
+gi.require_version("Vte", "3.91")
 
 from gi.repository import Gtk, GLib, Gio, Adw
 from apx_gui.windows.main_window import ApxGUIWindow
--- apx_gui/widgets/tab_subsystem.py
+++ apx_gui/widgets/tab_subsystem.py
@@ -17,7 +17,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Gtk, Gio, GLib, GObject, Adw
+from gi.repository import Gtk, Gdk, Gio, GLib, GObject, Adw, Vte, Pango
 from uuid import UUID
 
 from apx_gui.core.apx_entities import Subsystem
@@ -25,19 +25,24 @@ from apx_gui.core.run_async import RunAsync
 
 
 @Gtk.Template(resource_path="/org/vanillaos/apx-gui/gtk/tab-subsystem.ui")
-class TabSubsystem(Adw.PreferencesPage):
+class TabSubsystem(Gtk.Box):
     __gtype_name__: str = "TabSubsystem"
 
     row_status: Adw.ActionRow = Gtk.Template.Child()
     row_stack: Adw.ActionRow = Gtk.Template.Child()
     row_pkgmanager: Adw.ActionRow = Gtk.Template.Child()
     row_programs: Adw.ExpanderRow = Gtk.Template.Child()
-    row_console: Adw.ActionRow = Gtk.Template.Child()
     row_reset: Adw.ActionRow = Gtk.Template.Child()
     row_delete: Adw.ActionRow = Gtk.Template.Child()
-    btn_console: Gtk.Button = Gtk.Template.Child()
+    btn_toggle_console: Gtk.Button = Gtk.Template.Child()
+    btn_restart_console: Gtk.Button = Gtk.Template.Child()
     btn_reset: Gtk.Button = Gtk.Template.Child()
     btn_delete: Gtk.Button = Gtk.Template.Child()
+    box_console: Gtk.Box = Gtk.Template.Child()
+
+    console: Vte.Terminal = Vte.Terminal()
+    console_initialized: bool = False
+    gesture_controller = Gtk.GestureClick(button=Gdk.BUTTON_SECONDARY)
 
     def __init__(
         self, window: Adw.ApplicationWindow, subsystem: Subsystem, **kwargs
@@ -56,7 +61,8 @@ class TabSubsystem(Adw.PreferencesPage):
             f"({len(self.__subsystem.exported_programs)}) Exported Programs"
         )
 
-        self.btn_console.connect("clicked", self.__on_console_clicked)
+        self.btn_toggle_console.connect("clicked", self.__on_console_clicked)
+        self.btn_restart_console.connect("clicked", self.__on_reset_console_clicked)
         self.btn_reset.connect("clicked", self.__on_reset_clicked)
         self.btn_delete.connect("clicked", self.__on_delete_clicked)
 
@@ -67,6 +73,18 @@ class TabSubsystem(Adw.PreferencesPage):
             row.set_icon_name(program.get("Icon", "application-x-executable-symbolic"))
             self.row_programs.add_row(row)
 
+        console: Vte.Terminal = self.__create_console()
+        self.box_console.prepend(self.console)
+
+    def __create_console(self) -> Vte.Terminal:
+        self.console.set_halign(Gtk.Align.FILL)
+        self.console.set_valign(Gtk.Align.FILL)
+        self.console.set_hexpand(True)
+        self.console.set_vexpand(True)
+        self.console.set_cursor_blink_mode(Vte.CursorBlinkMode.ON)
+        self.console.set_mouse_autohide(True)
+        self.console.add_controller(self.gesture_controller)
+
     @property
     def aid(self) -> UUID:
         return self.__aid
@@ -75,8 +93,32 @@ class TabSubsystem(Adw.PreferencesPage):
     def subsystem(self) -> Subsystem:
         return self.__subsystem
 
+    def run_command(self, command: list) -> None:
+        self.console.spawn_sync(
+            Vte.PtyFlags.DEFAULT,
+            None,
+            command,
+            [],
+            GLib.SpawnFlags.DO_NOT_REAP_CHILD,
+            None,
+            None,
+        )
+
     def __on_console_clicked(self, button: Gtk.Button) -> None:
-        GLib.spawn_command_line_async(f"kgx -e apx {self.__subsystem.name} enter")
+        if self.box_console.get_visible():
+            self.box_console.hide()
+            self.btn_restart_console.hide()
+        else:
+            self.box_console.show()
+            self.btn_restart_console.show()
+
+        if not self.console_initialized:
+            self.run_command(self.__subsystem.enter_command)
+            self.console_initialized = True
+
+    def __on_reset_console_clicked(self, button: Gtk.Button) -> None:
+        self.console.reset(True, True)
+        self.run_command(self.__subsystem.enter_command)
 
     def __on_reset_clicked(self, button: Gtk.Button) -> None:
         def on_callback(result, *args) -> None:
